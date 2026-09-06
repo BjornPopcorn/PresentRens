5e31d1a66c6c3d3d7465c4b531d7e6ffea97ffdf
BjornPopcorn <bjornmika2004@gmail.com>
2026-09-06 23:42:08 +0200

    from 0.35 to 0.9

--- src/lib/YearGraph.svelte
+++ src/lib/YearGraph.svelte
@@ -6,7 +6,7 @@
   let canvas;
 
   // Simple LOWESS smoothing
-  function lowess(x, y, f = 0.35) {
+  function lowess(x, y, f = 0.9) {
     const n = x.length;
     const r = Math.floor(n * f);
     const ySmooth = [];
